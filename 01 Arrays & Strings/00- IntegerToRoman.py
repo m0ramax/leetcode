46bbc3bbf0671fb9a2b0b922eb425b06155c932b
miguelmoramax <miguelmoramaturana@gmail.com>
2024-08-22 16:22:45 -0400

    int to roman and Koko

---
+++ 01 Arrays & Strings/00- IntegerToRoman.py
@@ -0,0 +1,15 @@
+def IntToRoman(num: int) -> int:
+    roman = {1:'I',4:'IV', 5:'V', 9:'IX', 10:'X', 40:'XL',50:'L',90:'XC', 100:'C', 400: 'CD', 500: 'D',900:'CM' ,1000:'M'}
+    count = ''
+
+    for i in sorted(roman.keys(),reverse=True):
+        while num >= i:
+            count += roman[i]
+            num -=i
+    return count
+
+def romanizer(numbers):
+    return [IntToRoman(num) for num in numbers]
+
+
+print(romanizer([1,49,23]))  # 3
